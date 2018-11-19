32a06b5ec20a53177e0bf462a56c913110937a82
gregbin <1263818776@qq.com>
2018-11-19 21:48:04 +0800

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,25 +4,17 @@
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
-<body>
-<header class="navbar navbar-fixed-top navbar-inverse">
-    <div class="container">
-        <div class="col-md-offset-1 col-md-10">
-            <a href="/" id="logo">Sample App</a>
-            <nav>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="{{route('help')}}">帮助</a></li>
-                    <li><a href="#">登录</a></li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-</header>
+<body id="app">
+@include('layouts._header')
 
 <div class="container">
-    @include('shared._messages')
-    @yield('content')
-    @include('layouts._footer')
+    <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </div>
+
+<script src="/js/app.js"></script>
 </body>
 </html>
